8feb41865ec234f711b25df35cad5957d883e9fe
David <xldaviduson@gmail.com>
2020-05-22 19:41:54 +0200

    Imagen con animacion
    La imagen se mueve en el sitio, falta hacer que se mueve en una direccion

--- core/src/com/spritesheet/game/SpritesheetGame.java
+++ core/src/com/spritesheet/game/SpritesheetGame.java
@@ -1,33 +1,74 @@
 package com.spritesheet.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-public class SpritesheetGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+public class SpritesheetGame implements ApplicationListener {
+
+	private static final int FRAME_COLS = 6, FRAME_ROWS = 5;
+
+	Animation<TextureRegion> walkAnimation;
+	Texture walkSheet;
+	SpriteBatch spriteBatch;
+
+	float stateTime;
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+
+		walkSheet = new Texture(Gdx.files.internal("sprite-animation1.png"));
+
+		TextureRegion[][] tmp = TextureRegion.split(walkSheet,walkSheet.getWidth() / FRAME_COLS,walkSheet.getHeight() / FRAME_ROWS);
+
+		TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+		int index = 0;
+		for (int i = 0; i < FRAME_ROWS; i++) {
+			for (int j = 0; j < FRAME_COLS; j++) {
+				walkFrames[index++] = tmp[i][j];
+			}
+		}
+
+		walkAnimation = new Animation<TextureRegion>(0.025f, walkFrames);
+
+		spriteBatch = new SpriteBatch();
+		stateTime = 0f;
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void render() {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		stateTime += Gdx.graphics.getDeltaTime();
+
+		TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+		spriteBatch.begin();
+		spriteBatch.draw(currentFrame, 100, 100);
+		spriteBatch.end();
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void dispose() {
+		spriteBatch.dispose();
+		walkSheet.dispose();
 	}
 }
