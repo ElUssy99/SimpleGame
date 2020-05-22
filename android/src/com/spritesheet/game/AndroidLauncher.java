73fc4d0252b4e68cb05a9f72ab8e9f77b6dd1697
David <xldaviduson@gmail.com>
2020-05-22 18:40:34 +0200

    Inicio del juego

---
+++ android/src/com/spritesheet/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.spritesheet.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.spritesheet.game.SpritesheetGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpritesheetGame(), config);
+	}
+}
---
+++ desktop/src/com/spritesheet/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.spritesheet.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.spritesheet.game.SpritesheetGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpritesheetGame(), config);
+	}
+}
